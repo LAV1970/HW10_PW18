aee04743b2f408c550c3d637b25d201121f8d8d4
lomakina0612 <lomakina0612@gmail.com>
2024-02-02 17:30:35 +0100

    02_02_02

--- quotes_project/quotes/models.py
+++ quotes_project/quotes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+
+class Quote(models.Model):
+    text = models.TextField()
+    author = models.CharField(max_length=255)
+    tags = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.text
