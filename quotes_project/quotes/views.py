11456476594923e25f2a0f540fcae5daa2c0e164
lomakina0612 <lomakina0612@gmail.com>
2024-02-02 17:39:49 +0100

    02_02_03

--- quotes_project/quotes/views.py
+++ quotes_project/quotes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Quote
 
-# Create your views here.
+
+# Create your views here
+def quote_list(request):
+    quotes = Quote.objects.all()
+    return render(request, "quotes/quote_list.html", {"quotes": quotes})
