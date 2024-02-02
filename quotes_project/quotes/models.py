8b76c2867726ec699a65dd7019cc62a0dba8e71f
lomakina0612 <lomakina0612@gmail.com>
2024-02-02 18:12:11 +0100

    02_02_07

--- quotes_project/quotes/models.py
+++ quotes_project/quotes/models.py
@@ -7,7 +7,6 @@ class Quote(models.Model):
     text = models.TextField()
     author = models.CharField(max_length=255)
     tags = models.CharField(max_length=255)
-    app_label = "quotes"
 
     def __str__(self):
         return self.text
