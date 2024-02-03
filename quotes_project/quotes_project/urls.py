795881405dede0e0d779ba169c01461eca489982
lomakina0612 <lomakina0612@gmail.com>
2024-02-03 15:07:58 +0100

    03_02_9

--- quotes_project/quotes_project/urls.py
+++ quotes_project/quotes_project/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from quotes_project.quotes.views import SignUpView
+from quotes.views import SignUpView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
