b7b281f71ac595a6e6918d372d35bbcdbaa867fd
lomakina0612 <lomakina0612@gmail.com>
2024-02-03 17:56:11 +0100

    03_02_11

--- quotes_project/quotes/urls.py
+++ quotes_project/quotes/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import quote_list
 
 urlpatterns = [
-    path("", quote_list, name="quote_list"),
+    path(
+        "", quote_list, name="quote_list"
+    ),  # обратите внимание на точку с запятой здесь
 ]
--- quotes_project/quotes/views.py
+++ quotes_project/quotes/views.py
@@ -4,15 +4,20 @@ from django.contrib.auth import login
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
+from django.shortcuts import render
+from .models import Quote
+
+
+def quote_list(request):
+    quotes = Quote.objects.all()
+    return render(request, "quotes/quote_list.html", {"quotes": quotes})
 
 
 class SignUpView(CreateView):
     model = User
     form_class = UserCreationForm
     template_name = "registration/signup.html"
-    success_url = reverse_lazy(
-        "login"
-    )  
+    success_url = reverse_lazy("login")
     # Укажите URL для перенаправления после успешной регистрации
 
     def form_valid(self, form):
