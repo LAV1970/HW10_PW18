4067cd20c826e92c242434f5abfd5c73fbac9bf0
lomakina0612 <lomakina0612@gmail.com>
2024-02-05 16:13:36 +0100

    05_02_02

--- quotes_project/quotes_project/settings.py
+++ quotes_project/quotes_project/settings.py
@@ -60,7 +60,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            BASE_DIR / "quotes_project/quotes/templates/registation"
+            BASE_DIR / "quotes_project/quotes/templates/registration"
         ],  # Укажите полный путь к папке с шаблонами
         "APP_DIRS": True,
         "OPTIONS": {
